d68ca8971e7ef5a1b810f0189ddf594432a56ba3
vitaldent <vitaldentodon@gmail.com>
2023-04-18 14:52:36 -0500

    metodo async await

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ console.log ('hello world')
 
 const URL = 'https://api.thedogapi.com/v1/images/search'
 
-fetch(URL)
-    .then(res=> res.json())
-    .then(data =>{
-        const img = document.querySelector('img');
-        img.src = data [0].url;
-    });
+async function myDog (){
+    const res = await fetch(URL);
+    const data = await res.json();
+    const img = document.querySelector('img');
+        img.src = data [0].url
+}
+
+const myButton = document.querySelector("button");
+myButton.onclick = myDog;
