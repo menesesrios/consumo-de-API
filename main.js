c37715e897768a35bab56f680dc5e81701c43236
vitaldent <vitaldentodon@gmail.com>
2023-04-19 11:19:16 -0500

    endpoints and queryparameters

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 console.log ('hello world')
 
-const URL = 'https://api.thedogapi.com/v1/images/search'
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 
-async function myDog (){
-    const res = await fetch(URL);
+async function reload (){
+    const res = await fetch(API_URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-        img.src = data [0].url
-}
+    
+    console.log(data)
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
 
-const myButton = document.querySelector("button");
-myButton.onclick = myDog;
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+}
+reload()
