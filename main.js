2b40827bc9d1b5d3aaed43a260a8b04d114c68b9
vitaldent <vitaldentodon@gmail.com>
2023-04-20 09:36:07 -0500

    funcion del status code y manejo del error

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-console.log ('hello world')
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_3wgPVjpBH1k7ZgC42RrKcQuhQAQJ0DSWMLEdquC0wjxfEAdMkUcn5HYe1bCFJSbM';
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_3wgPVjpBH1k7ZgC42RrKcQuhQAQJ0DSWMLEdquC0wjxfEAdMkUcn5HYe1bCFJSbM';
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=2&api_key=live_3wgPVjpBH1k7ZgC42RrKcQuhQAQJ0DSWMLEdquC0wjxfEAdMkUcn5HYe1bCFJSbM';
 
-async function reload (){
-    const res = await fetch(API_URL);
+async function loadRandomDogs (){
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
     
+    console.log('random')
     console.log(data)
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
+    
 
     img1.src = data[0].url;
     img2.src = data[1].url;
-    img3.src = data[2].url;
+    
 }
-reload()
+
+async function loadFavoritesDogs (){
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    
+    console.log('favourites')
+    console.log(data)
+
+  
+}
+loadRandomDogs();
+loadFavoritesDogs();
